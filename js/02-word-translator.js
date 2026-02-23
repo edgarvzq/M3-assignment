203a4523203e38f218c67151457d5d8fe8112838
Edgar Vazquez <edgar.vzq.b@gmail.com>
2026-02-22 16:25:14 -0800

    Completed exercise 2

---
+++ js/02-word-translator.js
@@ -0,0 +1,10 @@
+let userCode = prompt('Enter language code (es, de, en, fr)')       // COMP690 Request first number
+if(userCode=='es') {                                                // Hello World in Spanish
+    window.alert('Hola Mundo')                                      // 
+}else if(userCode=='de') {                                          // Hello World in German 
+    window.alert('Hallo Welt')                                      //
+} else if(userCode=='fr') {                                         // Hello WOrld in French
+    window.alert('Bonjour le monde')                                //  
+} else {                                                            // If not the same or num1 is not greater, then num2 si greater
+    window.alert('Hello World')    
+}
